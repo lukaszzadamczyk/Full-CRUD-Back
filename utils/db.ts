e1893e740bb9857fb4746165e98518ae84c925d6
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-06-24 16:22:32 +0100

    Change config database.

--- utils/db.ts
+++ utils/db.ts
@@ -4,6 +4,7 @@ export const pool = createPool({
   host: "localhost",
   user: "root",
   password: "root",
+  port: 8889,
   database: "crud_user",
   namedPlaceholders: true,
   decimalNumbers: true,
