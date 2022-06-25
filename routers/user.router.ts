f2b320c3b18026ee456e3e99999440323e32f411
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-06-25 21:30:04 +0100

    Added update path and added new methods to record.

--- routers/user.router.ts
+++ routers/user.router.ts
@@ -13,6 +13,18 @@ export const userRouter = Router()
     res.json(user);
   })
   .delete("/delete/:id", async (req, res) => {
-    const removeUser = await UserRecord.remove(req.params.id);
+    const { id } = req.params;
+    const removeUser = await UserRecord.remove(id);
     res.json(removeUser);
+  })
+  .get("/get/:id", async (req, res) => {
+    const { id } = req.params;
+    const user = await UserRecord.getOneUser(id);
+    res.json(user);
+  })
+  .put("/update/:id", async (req, res) => {
+    const { id } = req.params;
+    const { name, email, contact } = req.body;
+    const user = new UserRecord({ name, email, contact, id });
+    await user.update();
   });
