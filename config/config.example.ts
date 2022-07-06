b922599a7ba17ead41d42d7c550ccd46e5d3544a
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-07-06 20:26:40 +0100

    Added config example.

---
+++ config/config.example.ts
@@ -0,0 +1,8 @@
+// export const config = {
+//   host: "localhost",
+//   user: "user",
+//   password: "password",
+//   port:8889,
+//   database: "database",
+//   corsOrigin: 'http://localhost:3000'
+// };
